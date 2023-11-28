9c067a13daf1d7a5298b7bac3416b3b2267c7777
ahmed elmelegy <ahmedelmelegy3570@gmail.com>
2023-11-28 17:23:04 +0200

    archive

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -3,7 +3,7 @@
 echo $imageName #getting Image name from env variable
 
 docker run --rm -v /home/ahmed/Library/Cache:/root/.cache/ -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName > trivy_scan_results.txt
-docker run --rm -v /home/ahmed/Library/Cache:/root/.cache/ -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light $imageName
+docker run --rm -v /home/ahmed/Library/Cache:/root/.cache/ -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light $imageName >> trivy_scan_results.txt
 
     # Trivy scan result processing
     exit_code=$?
